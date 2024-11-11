25747234314be55f0040d2765f694c15f56db2cb
Sean <144811089+5ean-h@users.noreply.github.com>
2024-11-11 00:05:36 +0000

    Update hangclient.c

--- hangclient.c
+++ hangclient.c
@@ -1,58 +1,105 @@
 /* Hangclient.c - Client for hangman server.  */
 
- #include <stdio.h>
- #include <sys/types.h>
- #include <sys/socket.h>
- #include <netinet/in.h>
- #include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+#include "DieWithMessage.h"
+#include "TCPClientUtility.h"
 
- # define LINESIZE 80
- # define HANGMAN_TCP_PORT 1066
+#define LINESIZE 80
+#define HANGMAN_TCP_PORT "5066"
 
- int main (int argc, char * argv [])
- {
- 	struct sockaddr_in server; /* Server's address assembled here */
- 	struct hostent * host_info;
- 	int sock, count;
- 	 char i_line[LINESIZE];
- 	 char o_line[LINESIZE];
- 	char * server_name;
+void DieWithMessage(const char* msg, const char* detail) {
+    if (detail) {
+        fprintf(stderr, "%s: %s\n", msg, detail);
+    }
+    else {
+        perror(msg);
+    }
+    exit(EXIT_FAILURE);
+}
 
- 	/* Get server name from the command line.  If none, use 'localhost' */
+int main(int argc, char* argv[])
+{
+    struct addrinfo hints, * server_info, * p;
+    int sock, rv;
+    char i_line[LINESIZE], o_line[LINESIZE];
+    char* server_name;
 
- 	server_name = (argc = 1)?  argv [1]: "localhost";
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <Server Name>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
- 	/* Create the socket */
- 	sock = socket (AF_INET, SOCK_STREAM, 0);
- 	if (sock <0) {
- 		perror ("Creating stream socket");
- 		exit (1);
- 	}
+    server_name = argv[1];
 
- 	host_info = gethostbyname(server_name);
- 	if (host_info == NULL) {
- 		fprintf (stderr, "%s: unknown host:%s \ n", argv [0], server_name);
- 		exit (2);
- 	}
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;     // Allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP socket
 
- 	/* Set up the server's socket address, then connect */
+    if ((rv = getaddrinfo(server_name, HANGMAN_TCP_PORT, &hints, &server_info)) != 0) {
+        DieWithMessage("getaddrinfo() failed", gai_strerror(rv));
+    }
 
- 	server.sin_family = host_info->h_addrtype;
- 	memcpy ((char *) & server.sin_addr, host_info->h_addr, host_info->h_length);
- 	server.sin_port = htons (HANGMAN_TCP_PORT);
 
- 	if (connect (sock, (struct sockaddr *) & server, sizeof server) <0) {
- 		perror ("connecting to server");
- 		exit (3);
- 	}
- 	/*OK connected to the server.  
- Take a line from the server and show it, take a line and send the user input to the server. 
- Repeat until the server terminates the connection. */
+    for (p = server_info; p != NULL; p = p->ai_next) {
+        if ((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            continue;
+        }
 
- 	printf ("Connected to server% s \ n", server_name);
- 	while ((count = read (sock, i_line, LINESIZE)) > 0) {
- 		write (1, i_line, count);
- 		count = read (0, o_line, LINESIZE);//0 = STDIN
- 		write (sock, o_line, count);
- 	}
- }
+        if (connect(sock, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sock);
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        DieWithMessage("Failed to connect", NULL);
+    }
+
+    freeaddrinfo(server_info);
+
+    printf("Connected to the server.\n");
+
+    while (1) {
+        printf("Enter your guess: ");
+        if (fgets(i_line, LINESIZE, stdin) == NULL) {
+            perror("fgets");
+            break;
+        }
+    
+        i_line[strcspn(i_line, "\n")] = 0;
+
+        if (write(sock, i_line, strlen(i_line)) == -1) {
+            perror("write");
+            break;
+        }
+
+        int bytes_read = read(sock, o_line, LINESIZE - 1);
+        if (bytes_read == -1) {
+            perror("read");
+            break;
+        }
+        else if (bytes_read == 0) {
+            printf("Server disconnected.\n");
+            break;
+        }
+        
+        o_line[bytes_read] = 0;
+        printf("Server response: %s\n", o_line);
+
+    }
+
+
+    close(sock);
+    return 0;
+
+}
