ed2675973e5898bc483ab7cbb23ed1dc967312ef
Sean <144811089+5ean-h@users.noreply.github.com>
2024-11-13 04:24:36 +0000

    Update TCPClientUtility.h

--- TCPClientUtility.h
+++ TCPClientUtility.h
@@ -1,6 +1,6 @@
 #ifndef TCPCLIENTUTILITY_H
 #define TCPCLIENTUTILITY_H
 
-int SetupTCPClientSocket(const char* host, const char* port);
+int SetupTCPClientSocket(const char *host, const char *port);
 
 #endif
