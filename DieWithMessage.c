ef5789e1845418625a43803e354ecaea0b571544
Sean <144811089+5ean-h@users.noreply.github.com>
2024-11-13 04:22:01 +0000

    Update DieWithMessage.c

--- DieWithMessage.c
+++ DieWithMessage.c
@@ -1,15 +1,8 @@
-/* DieWithMessage.c */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include "DieWithMessage.h"
 
-void DieWithMessage(const char *msg, const char *detail) {
-    if (detail) {
-        fprintf(stderr, "%s: %s\n", msg, detail);
-    } else {
-        perror(msg);
-    }
+void DieWithMessage(const char *message) {
+    perror(message);
     exit(EXIT_FAILURE);
 }
